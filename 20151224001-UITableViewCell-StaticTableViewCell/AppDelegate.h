5f139ac81dc7bd5b7cc7c539bbb83c1db0877a1d
raineryue <jinlei_yue@sina.com>
2015-12-24 15:43:46 +0800

    Initial Commit

---
+++ 20151224001-UITableViewCell-StaticTableViewCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20151224001-UITableViewCell-StaticTableViewCell
+//
+//  Created by Rainer on 15/12/24.
+//  Copyright © 2015年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20151224001-UITableViewCell-StaticTableViewCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20151224001-UITableViewCell-StaticTableViewCell
+//
+//  Created by Rainer on 15/12/24.
+//  Copyright © 2015年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
